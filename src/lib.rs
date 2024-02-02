2e6e93d4d487fa437686e055fd6fed5ce375e8bf
Sam Wyss <sjwyss@outlook.com>
2024-02-01 22:34:05 -0500

    finalized project structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
-// declare all local modules
-mod solver;
+// declare all local modules and conditionally expose them to main.rs
+pub mod solver;
 mod engine;
 mod geometry;
 mod helpers;
-
-// re-export solver to be visible in main.rs
-pub use solver::*;
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,6 @@ use fdtd_waveguide;
 
 fn main() {
     println!("Hello, world!");
+
+    fdtd_waveguide::solver::test();
 }
