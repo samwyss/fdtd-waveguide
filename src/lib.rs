778a8c5617d708a693c3ce0913c363dee869d564
Sam Wyss <sjwyss@outlook.com>
2024-02-02 15:09:26 -0500

    added potentially useful constants to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 // cargo crate imports
 use std::error::Error;
 
+// declare constants
+pub const ETA_0: f64 = 376.730313668; // [Ohms] free space impedance
+pub const EP_0: f64 = 8.8541878128e-12; // [F/m] free space vacuum permittivity
+pub const MU_0: f64 = 1.25663706212e-6; // [H/m] free space vacuum permeability
+pub const C_0: f64 = 299792458.0; // [m/s] free space speed of light
+
 // declare all local modules and conditionally expose them to main.rs
 mod engine;
 mod geometry;
