0cea973444dd93bce6d55f19a013663a11c7e4d2
Sam Wyss <sjwyss@outlook.com>
2024-02-08 20:45:11 -0500

    finished analysis viewer

--- analysis/analysis.jl
+++ analysis/analysis.jl
@@ -5,10 +5,10 @@ using DelimitedFiles
 data = readdlm("./ex.csv", ',', Float64, '\n')
 
 # constants
-time = 10
-x_size = 17
-y_size = 17
-z_size = 17
+#time = 4
+x_size = 67
+y_size = 67
+z_size = 67
 x_len = 0.5
 y_len = 0.5
 z_len = 0.5
@@ -20,21 +20,24 @@ x = LinRange(1, x_len, x_size)
 y = LinRange(1, y_len, y_size)
 z = LinRange(1, z_len, z_size)
 
+time = Observable(1)
+
 sgrid = SliderGrid(
     fig[2, 1],
     (label = "yz plane - x axis", range = 1:length(x)),
     (label = "xz plane - y axis", range = 1:length(y)),
     (label = "xy plane - z axis", range = 1:length(z)),
+    (label = "Time", range = 1:length(data[:,1])),
 )
 
 lo = sgrid.layout
 nc = ncols(lo)
 
 # slice data on timestep
-data_slice = data[time, :]
+data_slice = @lift(data[$time, :])
 
 # reorder into volume
-data_vol = reshape(data_slice, (x_size, y_size, z_size))
+data_vol = @lift(reshape($data_slice, (x_size, y_size, z_size)))
 
 vol = data_vol#[cos(X)*sin(Y)*sin(Z) for X ∈ x, Y ∈ y, Z ∈ z]
 plt = volumeslices!(ax, x, y, z, vol, colormap=:coolwarm, colorrange=(-0.1,0.1)) # interpolate=true
@@ -45,6 +48,29 @@ sl_yz, sl_xz, sl_xy = sgrid.sliders
 on(sl_yz.value) do v; plt[:update_yz][](v) end
 on(sl_xz.value) do v; plt[:update_xz][](v) end
 on(sl_xy.value) do v; plt[:update_xy][](v) end
+on(sgrid.sliders[4].value) do v 
+    time[] = v
+    # slice data on timestep
+    data_slice = @lift(data[$time, :])
+
+    # reorder into volume
+    data_vol = @lift(reshape($data_slice, (x_size, y_size, z_size)))
+
+    #vol = data_vol#[cos(X)*sin(Y)*sin(Z) for X ∈ x, Y ∈ y, Z ∈ z]
+    #plt = volumeslices!(ax, x, y, z, vol, colormap=:coolwarm, colorrange=(-0.1,0.1)) # interpolate=true
+
+    set_close_to!(sl_yz, .5length(x))
+    set_close_to!(sl_xz, .5length(y))
+    set_close_to!(sl_xy, .5length(z))
+
+    # add toggles to show/hide heatmaps
+    hmaps = [plt[Symbol(:heatmap_, s)][] for s ∈ (:yz, :xz, :xy)]
+    toggles = [Toggle(lo[i, nc + 1], active = true) for i ∈ 1:length(hmaps)]
+
+    map(zip(hmaps, toggles)) do (h, t)
+        connect!(h.visible, t.active)
+    end
+end
 
 set_close_to!(sl_yz, .5length(x))
 set_close_to!(sl_xz, .5length(y))
